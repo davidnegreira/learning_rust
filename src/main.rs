59a1c4f185fa7cea49a0d0d7c8b177bb377bb8ad
David Negreira Rios <david.negreirarios.ext@telefonica.com>
2018-12-28 09:17:07 +0100

    tabuladores

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,26 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-	println!("Adivina el número!!");
-	println!("Por favor dí un número: ");
-	let secret_number = rand::thread_rng().gen_range(1, 101);
-	
-	println!("El número secreto es:{}",secret_number);
+    println!("Adivina el número!!");
+    println!("Por favor dí un número: ");
 
-	let mut guess = String::new();
-	
-	io::stdin().read_line(&mut guess)
-		.expect("Eror al leer");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-	let guess: u32 = guess.trim().parse()
-		.expect("Por favor pon un número");
+    println!("El número secreto es:{}",secret_number);
 
-	println!("Has dicho: {}",guess);
+    let mut guess = String::new();
 
-	match guess.cmp(&secret_number) {
-		Ordering::Less => println!("Demasiado pequeño"),
-		Ordering::Greater => println!("Demasiado Alto"),
-		Ordering::Equal => println!("Ganas!!!"),
-	}
+    io::stdin().read_line(&mut guess)
+        .expect("Eror al leer");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Por favor pon un número");
+
+    println!("Has dicho: {}",guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Demasiado pequeño"),
+        Ordering::Greater => println!("Demasiado Alto"),
+        Ordering::Equal => println!("Ganas!!!"),
+    }
 }
